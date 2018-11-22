4d4b6112dfe2e654fd03b70cc02d58ed6d56e6cc
imgomez0127 <ianm0127@hotmail.com>
2018-11-22 17:26:09 -0500

    Added command line argument parsing to the ImageScraper file

--- ImageScraper.py
+++ ImageScraper.py
@@ -16,12 +16,12 @@ class ImageScraper(object):
 			imageCount (int): The amount of images that should be taken
 			fileName (str): The name of the output file
 			path (str): The name of the output path 
-			boundaries (:obj:4-tuple of :obj:int): a 4-tuple of integer values that represent the boundary box of the screenshot
+			boundary (:obj:4-tuple of :obj:int): a 4-tuple of integer values that represent the boundary box of the screenshot
 	"""
-	def __init__(self,imageCount,fileName="screenshot",path="./images",boundaries=()):
+	def __init__(self,imageCount,fileName="screenshot",path="./images",boundary=()):
 		self.__imageCount = imageCount
 		self.__fileName = fileName
-		self.__boundaries = boundaries
+		self.__boundary = boundary
 		self.__path = path
 		if(not os.path.exists(self.__path)):
 			try:
@@ -36,7 +36,7 @@ class ImageScraper(object):
 
 	@imageCount.setter
 	def imageCount(self,imageCount):
-		if(not isinstance(isinstance(imageCount,int))):
+		if(not isinstance(imageCount,int)):
 			try:
 				self.__imageCount = int(imageCount)
 			except:
@@ -70,16 +70,16 @@ class ImageScraper(object):
 	
 	@property
 	def boundary(self):
-	#boundaries (:obj:4-tuple of :obj:int): a 4-tuple of integer values that represent the boundary box of the screenshot
+	#boundary (:obj:4-tuple of :obj:int): a 4-tuple of integer values that represent the boundary box of the screenshot
 		return self.__boundary
 	
 	@boundary.setter
 	def boundary(self,boundary):
 		if(len(boundary) != 4):
-			raise ValueError("The boundaries take in 4 integer inputs")
+			raise ValueError("The boundary take in 4 integer inputs")
 		for val in boundary:
-			if(not isinstance(int,val)):
-				raise ValueError("The boundaries take in 4 integer inputs")
+			if(not isinstance(val,int)):
+				raise ValueError("The boundary take in 4 integer inputs")
 		self.__boundary = boundary
 
 	def takeScreenshots(self):
@@ -89,18 +89,34 @@ class ImageScraper(object):
 			in a jpg format and outputs it to the  given filePath
 		"""
 		for i in range(self.__imageCount):
-			screenshot = ImageGrab.grab(self.__boundaries)
+			screenshot = ImageGrab.grab(self.__boundary)
+			filePath = ""
+			if(self.__path[0] != "."):
+				filePath += "."
 			if(self.__path[-1] == "/"):
-				filePath = self.__path+self.__fileName+str(i+1)+".jpg"
+				filePath += self.__path + self.__fileName + str(i+1) + ".jpg"
 			else:
-				filePath = self.__path+'/' + self.__fileName + str(i) + ".jpg"
+				filePath += self.__path+'/' + self.__fileName + str(i) + ".jpg"
 			screenshot.save(filePath)
 			time.sleep(1)
 
 if __name__ == '__main__':
-	if(len(sys.argv)<1 or len(sys.argv) > 7):
+	if(len(sys.argv)<2 or len(sys.argv) > 8):
 		print("Usage: python ImageScraper.py <ImageCount> [fileName] [path] [boundary 1] [boundary 2] [boundary 3] [boundary 4]")
 		sys.exit(1)
-
-	Scraper = ImageScraper(5)
+	Scraper = ImageScraper(int(sys.argv[1]))
+	if(len(sys.argv)>=3):
+		Scraper.fileName=sys.argv[2]
+	if(len(sys.argv)>=4):
+		Scraper.path=sys.argv[3]
+	
+	if(len(sys.argv)>4 and len(sys.argv) == 8):
+		try:
+			Scraper.boundary=(int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]),int(sys.argv[7]))
+		except:
+			print("Boundary values must be integers.")
+			sys.exit(1)
+	elif(len(sys.argv)>4 and len(sys.argv)!=8):
+		print("Usage: python ImageScraper.py <ImageCount> [fileName] [path] [boundary 1] [boundary 2] [boundary 3] [boundary 4]")
+		sys.exit(1)
 	Scraper.takeScreenshots()
